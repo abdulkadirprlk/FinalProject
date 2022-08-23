d5b8427b807617440ef3878c46c72ff180498883
Abdulkadir Parlak <abdulkadirparlak3@gmail.com>
2022-08-23 11:18:43 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal; //Method injection
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //Bir iş sınıfı başka sınıfları new'lemez!!!
+            //iş kodları.  Yetkisi var mı? vb.
+
+            return _productDal.GetAll();
+        }
+    }
+}
