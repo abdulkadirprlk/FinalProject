ad0df904efe07de0981a2a42396422f3243c4c26
Abdulkadir Parlak <abdulkadirparlak3@gmail.com>
2022-08-29 13:28:02 +0300

    FinalProject 8.gün ders sonu

---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context : DB tabloları ile proje classlarını bağlamak.
+    public class NorthwindContext : DbContext
+    {
+        //projenin hangi veritabanı ile ilişkili olduğunu belirlediğimiz yer.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+
+        //projedeki nesneler ile veri tabanındaki tabloları eşleştirdiğimiz yer.
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
